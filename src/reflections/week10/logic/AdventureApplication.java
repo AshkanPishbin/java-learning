b1095047a597a16de7914ab11e0c4f1c83121309
AshkanPishbin <ashkan.pb@gmail.com>
2019-01-25 10:21:05 +0100

    stream

--- src/reflections/week10/logic/AdventureApplication.java
+++ src/reflections/week10/logic/AdventureApplication.java
@@ -1,4 +1,9 @@
 package reflections.week10.logic;
 
 public class AdventureApplication {
+    public static void main(String[] args) {
+        AventureSommerizer aventureSommerizer = new AventureSommerizer();
+        aventureSommerizer.displayNumberOfWords();
+        aventureSommerizer.displayNumberOfDistinctWord();
+    }
 }
--- src/reflections/week10/logic/AventureSommerizer.java
+++ src/reflections/week10/logic/AventureSommerizer.java
@@ -1,4 +1,31 @@
 package reflections.week10.logic;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public class AventureSommerizer {
-}
+
+    private List<String> lines=new AdventureReader().getLine();
+
+        public void displayNumberOfWords() {
+
+            long count = lines.stream()
+                    .map(e -> e.split(" "))
+                    .flatMap(Stream::of)
+                    .count();
+            System.out.println(count);
+        }
+
+
+        public void  displayNumberOfDistinctWord() {
+
+            long count = lines.stream()
+                    .map(e -> e.split(" "))
+                    .flatMap(Stream::of)
+                    .distinct()
+                    .count();
+            System.out.println(count);
+
+        }
+    }
+
