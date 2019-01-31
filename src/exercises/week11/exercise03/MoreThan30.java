3f712518113657ee0bf8601e3bbe02f350c93103
AshkanPishbin <ashkan.pb@gmail.com>
2019-01-31 19:46:41 +0100

    extend of time

--- src/exercises/week11/exercise03/MoreThan30.java
+++ src/exercises/week11/exercise03/MoreThan30.java
@@ -1,4 +1,22 @@
 package exercises.week11.exercise03;
 
-public class moreThan30 {
+import java.util.Map;
+
+public class MoreThan30 extends Time {
+    private NumberToWord numberToWord = new NumberToWord();
+    private Map<Integer, String> numbers = numberToWord.getNumbers();
+
+    @Override
+    public Boolean isInRange(Integer minute) {
+        if (minute > 30 && minute < 60) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getMessage(Integer hour, Integer minute) {
+        return numbers.get(60-minute) + " to " + numbers.get(hour+1);
+
+    }
 }
