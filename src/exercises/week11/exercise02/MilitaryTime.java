b932ba96e36790fb3d9834a645801a75f3c50019
AshkanPishbin <ashkan.pb@gmail.com>
2019-01-31 19:45:28 +0100

    convert am-pm to 24-hour

--- src/exercises/week11/exercise02/MilitaryTime.java
+++ src/exercises/week11/exercise02/MilitaryTime.java
@@ -2,16 +2,14 @@ package exercises.week11.exercise02;
 
 import java.time.format.DateTimeFormatter;
 
-public class MiltaryTime {
+public class MilitaryTime {
     private DateTimeFormatter format12 = DateTimeFormatter.ofPattern("hh:mm:ss a");
     private DateTimeFormatter format24 = DateTimeFormatter.ofPattern("HH:mm:ss");
 
     public String toMilitaryTime(String time) {
-
         return format24.format(format12.parse(time));
     }
-
 }
 
 
-}
+
