14f5ee7cc9c202ec5cfc4704a66007429fe9ca4c
AshkanPishbin <ashkan.pb@gmail.com>
2019-01-31 19:48:38 +0100

    corner cases

--- src/exercises/week11/exercise03/ConverterTest.java
+++ src/exercises/week11/exercise03/ConverterTest.java
@@ -1,5 +1,13 @@
-import static org.junit.jupiter.api.Assertions.*;
+package exercises.week11.exercise03;
+
+import org.junit.jupiter.api.Test;
 
 class ConverterTest {
+    private Converter converter = new Converter();
 
+    @Test
+    void toWord() {
+        String result = converter.toWord(24, 00);
+        System.out.println(result);
+    }
 }
